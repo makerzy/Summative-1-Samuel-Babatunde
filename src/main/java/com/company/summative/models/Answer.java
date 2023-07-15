e94c3a5461b9c0d62b78ee1d1e8e43c919173607
makerzy <botassammzy@gmail.com>
2023-07-15 13:31:10 -0700

    reorg code

--- src/main/java/com/company/summative/models/Answer.java
+++ src/main/java/com/company/summative/models/Answer.java
@@ -4,7 +4,17 @@ import java.util.Objects;
 
 public class Answer {
 
+
+    static int count = 0;
+    private int id;
+
     private String question;
+    private String answer;
+    public Answer(String ans){
+        this.answer = ans;
+        count +=1;
+        id = count;
+    }
 
     public String getQuestion() {
         return question;
@@ -26,35 +36,27 @@ public class Answer {
         return id;
     }
 
-
-    private String answer;
-
-    @Override
-    public String toString() {
-        return "Magic8{" +
-                "question='" + question + '\'' +
-                ", answer='" + answer + '\'' +
-                ", id=" + id +
-                '}';
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Answer answer = (Answer) o;
-        return Objects.equals(question, answer.question) && Objects.equals(this.answer, answer.answer);
+        Answer answer1 = (Answer) o;
+        return id == answer1.id && Objects.equals(question, answer1.question) && Objects.equals(answer, answer1.answer);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(question, answer);
+        return Objects.hash(id, question, answer);
     }
-private int id;
-    static int count = 0;
-    public Answer(String ans){
-        this.answer = ans;
-        count +=1;
-        id = count;
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", question='" + question + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
     }
+
+
 }
--- src/main/java/com/company/summative/models/Question.java
+++ src/main/java/com/company/summative/models/Question.java
@@ -1,8 +1,12 @@
 package com.company.summative.models;
 
+import java.util.Objects;
+
 public class Question {
 
     public String question;
+
+    public Question (){}
 
     public void setQuestion(String question) {
         this.question = question;
@@ -10,5 +14,25 @@ public class Question {
 
     public String getQuestion() {
         return question;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question1 = (Question) o;
+        return Objects.equals(question, question1.question);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question);
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "question='" + question + '\'' +
+                '}';
     }
 }
